182186d5feaeb9c5cb3389e638176ae7a062b95a
keyready <vip.korchak@list.ru>
2023-12-02 22:38:35 +0300

    IVT-35: допил крошки, чтобы не было релоуда страницы, убрал dist с backend

--- client/src/shared/UI/Breadcrumb/ui/Breadcrumb.tsx
+++ client/src/shared/UI/Breadcrumb/ui/Breadcrumb.tsx
@@ -1,8 +1,10 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { memo, useMemo } from 'react';
 import { BreadCrumb } from 'primereact/breadcrumb';
-import { MenuItem } from 'primereact/menuitem';
+import { MenuItem, MenuItemCommandEvent } from 'primereact/menuitem';
 import { HomeIcon } from '@radix-ui/react-icons';
+import { AppLink } from 'shared/UI/AppLink';
+import { Link, useNavigate } from 'react-router-dom';
 import classes from './Breadcrumb.module.scss';
 
 export interface BreadcrumbPath {
@@ -19,10 +21,17 @@ export const Breadcrumb = memo((props: BreadcrumbProps) => {
     const { className, path } = props;
 
     const home = useMemo<MenuItem>(() => ({ url: '/', icon: <HomeIcon /> }), []);
+    const navigate = useNavigate();
 
     return (
         <BreadCrumb
-            model={path}
+            model={path.map((pathPiece) => ({
+                ...pathPiece,
+                command(event: MenuItemCommandEvent) {
+                    event.originalEvent.preventDefault();
+                    navigate(pathPiece.url || '');
+                },
+            }))}
             home={home}
             className={classNames(classes.Breadcrumb, {}, [className])}
         />
--- client/src/widgets/PageTitle/ui/PageTitle.tsx
+++ client/src/widgets/PageTitle/ui/PageTitle.tsx
@@ -9,19 +9,17 @@ import classes from './PageTitle.module.scss';
 interface PageTitleProps {
     className?: string;
     title: string;
-    breadcrumb?: boolean;
     breadcrumbPath?: BreadcrumbPath[];
 }
 
 export const PageTitle = memo((props: PageTitleProps) => {
-    const { className, title, breadcrumb = true, breadcrumbPath } = props;
+    const { className, title, breadcrumbPath } = props;
 
     return (
         <VStack justify="start" className={classNames(classes.PageTitle, {}, [className])}>
             <Card className={classes.card}>
-                {breadcrumb && breadcrumbPath?.length && <Breadcrumb path={breadcrumbPath} />}
-                <Text align="left" size="large" className={classes.textBlock}
-title={title} />
+                {breadcrumbPath?.length && <Breadcrumb path={breadcrumbPath} />}
+                <Text align="left" size="large" className={classes.textBlock} title={title} />
             </Card>
         </VStack>
     );
